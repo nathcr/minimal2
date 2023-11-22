aa91725f34f9f0e1a09f7c627a2e399c53101b32
nathcr <142260582+nathcr@users.noreply.github.com>
2023-11-22 15:02:35 -0600

    Update __init__.py

--- minimal/__init__.py
+++ minimal/__init__.py
@@ -7,14 +7,6 @@ def my_mul(x, y):
     """A function that multiply. """
     return x*y
 
-from sympy import init_printing, symbols
-from sympy import diff, exp
-x, y = symbols('x y')
-
-def rtang(x1,x0):
-  expr=diff(x1)
-  a=expr.subs({x:x0})
-  return (y-x0,a*(x-x0))
 
 
 
